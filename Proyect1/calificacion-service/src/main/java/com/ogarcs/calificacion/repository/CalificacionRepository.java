e8d91375b03c3e1bc6a3ba2e1ffe1d5860a46177
Omar Darío García Acevedo <omar_garcs@Omars-MacBook-Pro.local>
2023-07-30 09:58:32 -0600

    Add 2 methods

--- Proyect1/calificacion-service/src/main/java/com/ogarcs/calificacion/repository/CalificacionRepository.java
+++ Proyect1/calificacion-service/src/main/java/com/ogarcs/calificacion/repository/CalificacionRepository.java
@@ -3,5 +3,11 @@ package com.ogarcs.calificacion.repository;
 import com.ogarcs.calificacion.entity.Calificacion;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CalificacionRepository extends MongoRepository<Calificacion, Long> {
+
+    List<Calificacion> findByUsuarioId(String usuarioId);
+
+    List<Calificacion> findByHotelId(String hotelId);
 }
