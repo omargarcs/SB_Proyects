6095479232b12fceab89f23c21706129639d9cfb
Omar Darío García Acevedo <omar_garcs@Omars-MacBook-Pro.local>
2023-07-30 08:39:47 -0600

    Init Commit CalificacionService

---
+++ Proyect1/calificacion-service/src/test/java/com/ogarcs/calificacion/CalificacionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogarcs.calificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalificacionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
